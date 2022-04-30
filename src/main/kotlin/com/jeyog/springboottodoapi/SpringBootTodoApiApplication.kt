adfb6d77c7e7293c3e36aa25abed704b9ecfd5ee
jeyog <choijy8816@gmail.com>
2022-04-30 16:50:08 +0900

    First commit

---
+++ src/main/kotlin/com/jeyog/springboottodoapi/SpringBootTodoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jeyog.springboottodoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootTodoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootTodoApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/jeyog/springboottodoapi/SpringBootTodoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jeyog.springboottodoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootTodoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
