abdbf146e08f8b3f982c62d71e99a62a6fb15328
jeyog <choijy8816@gmail.com>
2022-05-08 16:40:35 +0900

    Build: allOpen, noArg 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 	id("org.springframework.boot") version "2.6.7"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	id("org.asciidoctor.convert") version "1.5.8"
+	id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
+	id("org.jetbrains.kotlin.plugin.noarg") version "1.6.21"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
@@ -23,12 +25,22 @@ repositories {
 	mavenCentral()
 }
 
+allOpen {
+	annotation("javax.persistence.Entity")
+}
+
+noArg {
+	annotation("javax.persistence.Entity")
+}
+
+
 var snippetsDir by extra { File("build/generated-snippets") }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
